4f3b30c46cc808e38b69f8fef3d5469143bc1e06
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2025-11-02 20:26:27 -0800

    Implemented maxDiff method with array

--- src/Practice.java
+++ src/Practice.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
+
 public class Practice {
     /**
      * Returns the difference between the largest and smallest integer in an array.
@@ -6,12 +10,29 @@ public class Practice {
      * @return the difference between the largest and smallest number
      */
     public static int maxDiff(int[] nums) {
-        // TODO: implement this
-        return -1;
+        if(nums.length == 0){
+            return -1;
+        }
+
+        int min = nums[0];
+        int max = nums[0];
+
+        for(int num : nums)
+        {
+            if(num < min)
+            {
+                min = num;
+            }
+            if(num > max)
+            {
+                max = num;
+            }
+        }
+        int diff = max - min;
+        return diff;
     }
 
-
-    // TODO: Implement the other methods from the study guide AND tests for each one
+    // Implement the other methods from the study guide AND tests for each one
 
     // For each method you are only required to implement it for one of the data
     // structures. But use a different data structure for each method. For example,
