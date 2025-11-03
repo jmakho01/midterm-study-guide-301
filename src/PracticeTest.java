2c84d58501f78b7f932930cc197f0688a5efffa8
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2025-11-02 21:23:01 -0800

    Added additional tests for maxDiff method

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 public class PracticeTest {
 
-    // TODO: Make more tests for maxDiff
+    // Make more tests for maxDiff
     @Test
     void testMaxDiffPositiveAndNegative() {
         // Arrange
@@ -17,9 +17,29 @@ public class PracticeTest {
         // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
         assertEquals(14, actual);
     }
-    
 
-    // TODO: Make tests for each problem you solve
+    @Test
+    void testMaxDiffLargeNums() {
+        int[] numbers = {28, 22, 49, 55};
+        int actual = Practice.maxDiff(numbers);
+        assertEquals(33, actual);
+    }
+
+    @Test
+    void testMaxDiffSmallNums() {
+        int[] numbers = {5, 2, 9, 4};
+        int actual = Practice.maxDiff(numbers);
+        assertEquals(7, actual);
+    }
+
+    @Test
+    void testMaxDiffEdgeCase() {
+        int[] numbers = {};
+        int actual = Practice.maxDiff(numbers);
+        assertEquals(-1, actual);
+    }
+    
+    // Make tests for each problem you solve
     
 }
 
